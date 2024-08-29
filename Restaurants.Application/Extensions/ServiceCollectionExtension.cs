51e87f80f0bd3a990035399660ae736cc87fa3bd
Max Ali <mohseni716015@gmail.com>
2024-08-29 08:21:53 +0200

    done with 5-API Controller

---
+++ Restaurants.Application/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Application.Restaurants;
+
+namespace Restaurants.Application.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        var applicationAssembly = typeof(ServiceCollectionExtension).Assembly; 
+
+        services.AddScoped<IRestaurantsService, RestaurantsService>();
+
+        services.AddAutoMapper(applicationAssembly);
+
+        //services.AddAutoMapper(typeof(ServiceCollectionExtension).Assembly);
+        services.AddValidatorsFromAssembly(applicationAssembly)
+            .AddFluentValidationAutoValidation();
+    }
+    
+} 
---
+++ Restaurants.Infrastructure/Extentions/ServiceCollectionExtentions.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Persistence;
+using Restaurants.Infrastructure.Repository;
+using Restaurants.Infrastructure.Seeders;
+
+namespace Restaurants.Infrastructure.Extentions; 
+
+public static class ServiceCollectionExtentions 
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration) {
+        var connectionString = configuration.GetConnectionString("RestaurantsDb");
+        services.AddDbContext<RestaurantsDbContext>( options => options.UseSqlServer(connectionString));
+
+        
+        services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
+        services.AddScoped<IRestaurantsRepository, RestaurantsRepository >();
+    }
+}
