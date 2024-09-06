b976cb6788efa73278c06201557fb2180059dc6c
Max Ali <mohseni716015@gmail.com>
2024-09-06 11:08:34 +0200

    Extracting presentation layer services part 53

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -4,85 +4,20 @@ using Restaurants.Application.Extensions;
 using Serilog;
 using Restaurants.API.Middlewares;
 using Restaurants.Domain.Entities;
-using Microsoft.OpenApi.Models;
+using Restaurants.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen( c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-    });
-    c.AddSecurityRequirement( new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference 
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "bearerAuth"
-                } 
-                
-            },[]
-        }
-    });
-
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 
 //builder.Configuration.GetConnectionString("RestaurantsDb"); //passa denna till AddInfrastructure
 //add dbcontext 
 //builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString);
+builder.AddPresentation();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Host.UseSerilog((context, configuration) => 
-    configuration.ReadFrom.Configuration(context.Configuration)
-
-    /*
-        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-        .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}")
-    */ 
-
-    /* Flyssta till appsetings
-
-    "Serilog": {
-        "MinimumLevel": {
-            "Override": {
-                "Microsoft": "Warning",
-                "Microsoft.EntityFrameworkCore": "Information"
-            }
-            },
-            "WriteTo": [
-            {
-                "Name": "Console",
-                "Args": {
-                "outputTemplate": "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}"
-                }
-            },
-            {
-                "Name": "File",
-                "Args": {
-                "path": "Logs/Restaurant-Api-.log",
-                "rollingInterval": "Day",
-                "rollOnFileSizeLimit": true,
-                "formatter": "Serilog.Formatting.Compact.CompactJsonFormatter, Serilog.Formatting.Compact"
-                }
-            }
-            ]
-        }
-    */
-);
 
 var app = builder.Build();
 
