280a34c84bfeebb0422e2a5e9f5c2f458d441407
Max Ali <mohseni716015@gmail.com>
2024-09-09 16:53:07 +0200

    Added enfoce authication based on the users claim

--- Restaurants.Infrastructure/Extentions/ServiceCollectionExtentions.cs
+++ Restaurants.Infrastructure/Extentions/ServiceCollectionExtentions.cs
@@ -29,5 +29,7 @@ public static class ServiceCollectionExtentions
         services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
         services.AddScoped<IRestaurantsRepository, RestaurantsRepository >();
         services.AddScoped<IDishesRepository, DishesRepository>();
+        services.AddAuthorizationBuilder()
+            .AddPolicy(PolicyNames.HasNationality, builder => builder.RequireClaim(AppClaimTypes.Nationality, "Svensk", "Norsk"));
     }
 }
